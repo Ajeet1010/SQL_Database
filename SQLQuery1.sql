47d2208ca32515e474a1d4a4c924266ae80ff3b4
Ajeet <ajeetpal101999@gmail.com>
2023-04-14 22:32:36 +0530

    [Ajeet] Creating a table with some columns.

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,1 +1,6 @@
+-----  UC1 - Creating a database of Customer Practice  -----
 create database Customer_Practice
+
+-----  UC2 - Creating a Customer table with some column in it.  -----
+create table Customer(Id int Identity(1,1) primary key,  
+Name varchar(50), PhoneNo int, Address varchar(50));
